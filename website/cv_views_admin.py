f3c0ebf4edb78563865b06f0542afad59f24b4ae
Robert <61035696+robertborowski@users.noreply.github.com>
2023-09-23 18:17:55 -0400

    small

--- website/cv_views_admin.py
+++ website/cv_views_admin.py
@@ -11,22 +11,18 @@
 from website.backend.uuid_timestamp import create_uuid_function, create_timestamp_function
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user, logout_user
-from website.backend.candidates.redis import redis_check_if_cookie_exists_function, redis_connect_to_database_function
 from website import db
-from website.backend.candidates.user_inputs import alert_message_default_function_v2
 import os
-from website.backend.selenium_script import reddit_scrape_function
-from website.models import RedditPostsObj, PollsObj, RedditMappingObj, HostMarketingObj, EmailSentObj
-from website.backend.candidates.user_inputs import sanitize_email_function
 from datetime import datetime
-from website.backend.candidates.send_emails import send_email_template_function
+from website.backend.connection import redis_connect_open_function
+from website.backend.alerts import get_alert_message_function
 # ------------------------ imports end ------------------------
 
 # ------------------------ function start ------------------------
 polling_views_admin = Blueprint('polling_views_admin', __name__)
 # ------------------------ function end ------------------------
 # ------------------------ connect to redis start ------------------------
-redis_connection = redis_connect_to_database_function()
+redis_connection = redis_connect_open_function()
 # ------------------------ connect to redis end ------------------------
 
 # ------------------------ individual route start ------------------------
@@ -45,175 +41,15 @@ def admin_function(url_redirect_code=None):
       url_redirect_code = request.args.get('url_redirect_code')
     except:
       pass
-  alert_message_dict = alert_message_default_function_v2(url_redirect_code)
+  alert_message_dict = get_alert_message_function(url_redirect_code)
   page_dict = {}
   page_dict['alert_message_dict'] = alert_message_dict
   # ------------------------ page dict end ------------------------
-  # ------------------------ submission start ------------------------
-  if request.method == 'POST':
-    # ------------------------ get ui start ------------------------
-    ui_reddit_input = request.form.get('ui_reddit_input')
-    ui_poll_input = request.form.get('ui_poll_input')
-    ui_reddit_api_button_posts_and_comments = request.form.get('ui_reddit_api_button_posts_and_comments')
-    ui_reddit_api_button_send_messages = request.form.get('ui_reddit_api_button_send_messages')
-    # ------------------------ get ui end ------------------------
-    # ------------------------ reddit poll mapping start ------------------------
-    if ui_reddit_input != None and ui_poll_input != None:
-      obj_reddit = RedditPostsObj.query.filter_by(id=ui_reddit_input).first()
-      obj_poll = PollsObj.query.filter_by(id=ui_poll_input).first()
-      if obj_reddit != None and obj_reddit != [] and obj_poll != None and obj_poll != []:
-        # ------------------------ check if already mapped start ------------------------
-        obj_map = RedditMappingObj.query.filter_by(fk_poll_id=ui_poll_input,fk_reddit_post_id=ui_reddit_input).first()
-        if obj_map != None and ui_reddit_input != []:
-          return redirect(url_for('polling_views_admin.admin_function', url_redirect_code='e45'))
-        # ------------------------ check if already mapped end ------------------------
-        else:
-          # ------------------------ insert to db start ------------------------
-          new_row = RedditMappingObj(
-            id=create_uuid_function('map_'),
-            created_timestamp=create_timestamp_function(),
-            fk_poll_id=ui_poll_input,
-            fk_reddit_post_id=ui_reddit_input
-          )
-          db.session.add(new_row)
-          db.session.commit()
-          # ------------------------ insert to db end ------------------------
-          return redirect(url_for('polling_views_admin.admin_function', url_redirect_code='s21'))
-    # ------------------------ reddit poll mapping end ------------------------
-    # ------------------------ reddit posts and comments start ------------------------
-    if ui_reddit_api_button_posts_and_comments == 'on':
-      return redirect(url_for('polling_views_admin.admin_function', url_redirect_code='s21'))
-    # ------------------------ reddit posts and comments end ------------------------
-  # ------------------------ submission end ------------------------    
   print(' ------------- 100-admin start ------------- ')
   page_dict = dict(sorted(page_dict.items(),key=lambda x:x[0]))
   for k,v in page_dict.items():
     print(f"k: {k} | v: {v}")
     pass
   print(' ------------- 100-admin end ------------- ')
-  return render_template('polling/admin_templates/dashboard/index.html', page_dict_html=page_dict)
-# ------------------------ individual route end ------------------------
-
-# ------------------------ individual route start ------------------------
-@polling_views_admin.route('/admin/hosts', methods=['GET', 'POST'])
-@polling_views_admin.route('/admin/hosts/', methods=['GET', 'POST'])
-@polling_views_admin.route('/admin/hosts/<url_redirect_code>', methods=['GET', 'POST'])
-@login_required
-def admin_hosts_function(url_redirect_code=None):
-  # ------------------------ check admin status start ------------------------
-  if current_user.email != os.environ.get('RUN_TEST_EMAIL'):
-    return redirect(url_for('polling_views_interior.polling_dashboard_function'))
-  # ------------------------ check admin status end ------------------------
-  # ------------------------ page dict start ------------------------
-  if url_redirect_code == None:
-    try:
-      url_redirect_code = request.args.get('url_redirect_code')
-    except:
-      pass
-  alert_message_dict = alert_message_default_function_v2(url_redirect_code)
-  page_dict = {}
-  page_dict['alert_message_dict'] = alert_message_dict
-  # ------------------------ page dict end ------------------------
-  # ------------------------ submission start ------------------------
-  if request.method == 'POST':
-    # ------------------------ add host email start ------------------------
-    try:
-      # ------------------------ get ui start ------------------------
-      ui_host_podcast_name = request.form.get('ui_host_podcast_name')
-      ui_host_podcast_email = request.form.get('ui_host_podcast_email')
-      ui_host_podcast_greeting = request.form.get('ui_host_podcast_greeting')
-      # ------------------------ get ui end ------------------------
-      # ------------------------ sanitize inputs start ------------------------
-      sanitize_email_function_check = sanitize_email_function(ui_host_podcast_email)
-      if sanitize_email_function_check == False:
-        return redirect(url_for('polling_views_admin.admin_hosts_function', url_redirect_code='e6'))
-      # ------------------------ sanitize inputs end ------------------------
-      # ------------------------ check if exists start ------------------------
-      db_obj = HostMarketingObj.query.filter_by(host_email=ui_host_podcast_email).first()
-      if db_obj == None or db_obj == []:
-        # ------------------------ add to db start ------------------------
-        try:
-          new_row = HostMarketingObj(
-            id = create_uuid_function('host_'),
-            created_timestamp = create_timestamp_function(),
-            podcast_name = ui_host_podcast_name,
-            host_email = ui_host_podcast_email,
-            greeting_name = ui_host_podcast_greeting,
-            unsubscribed = False
-          )
-          db.session.add(new_row)
-          db.session.commit()
-          return redirect(url_for('polling_views_admin.admin_hosts_function', url_redirect_code='s10'))
-        except:
-          pass
-        # ------------------------ add to db end ------------------------
-      # ------------------------ check if exists end ------------------------
-    except:
-      pass
-    # ------------------------ add host email end ------------------------
-    # ------------------------ send emails function start ------------------------
-    try:
-      ui_send_emails = request.form.get('ui_send_emails')
-      if ui_send_emails == 'on':
-        db_host_objs = HostMarketingObj.query.filter_by(unsubscribed=False).order_by(HostMarketingObj.created_timestamp.desc()).all()
-        for i_obj in db_host_objs:
-          # ------------------------ testing start ------------------------
-          # if i_obj.host_email != os.environ.get('PERSONAL_EMAIL'):
-          #   continue
-          # ------------------------ testing end ------------------------
-          # ------------------------ set variables start ------------------------
-          i_send = False
-          i_subject = 'Audience Insights and Sentiment'
-          i_day_spacing_limit = 3
-          current_date = datetime.now().date()
-          # ------------------------ set variables end ------------------------
-          # ------------------------ spam check #1 start ------------------------
-          db_email_obj = EmailSentObj.query.filter_by(to_email=i_obj.host_email,subject=i_subject).order_by(EmailSentObj.created_timestamp.desc()).first()
-          if db_email_obj == None or db_email_obj == []:
-            i_send = True
-          else:
-            date_diff = (current_date - db_email_obj.created_timestamp.date()).days
-            if date_diff >= i_day_spacing_limit:
-              i_send = True
-          # ------------------------ spam check #1 end ------------------------
-          # ------------------------ good to send start ------------------------
-          if i_send == True:
-            # ------------------------ send email start ------------------------
-            try:
-              output_body = f"<p>Hi {i_obj.greeting_name},</p>\
-                              <p>I appreciate the exceptional content that you create. I aggregate audience data from X (Twitter), Reddit, and YouTube, to provide you with a comprehensive hub for analyzing your podcasts' audience trends & feedback. Explore <a href='https://herdreviews.com/'>HerdReviews</a>, the podcast polling platform, to gain valuable insights about your audience personality trends.</p>\
-                              <p style='margin:0px;'>Best,</p>\
-                              <p style='margin:0px;'>HerdReviews Support Team</p>\
-                              <p style='margin:0px;font-size:10px'>Reply 'stop' to unsubscribe</p>"
-              send_email_template_function(i_obj.host_email, i_subject, output_body)
-            except:
-              pass
-            # ------------------------ send email end ------------------------
-            # ------------------------ insert email to db start ------------------------
-            try:
-              new_row_email = EmailSentObj(
-                id = create_uuid_function('email_'),
-                created_timestamp = create_timestamp_function(),
-                from_user_id_fk = current_user.id,
-                to_email = i_obj.host_email,
-                subject = i_subject,
-                body = output_body
-              )
-              db.session.add(new_row_email)
-              db.session.commit()
-            except:
-              pass
-            # ------------------------ insert email to db end ------------------------
-          # ------------------------ good to send end ------------------------
-      return redirect(url_for('polling_views_admin.admin_hosts_function', url_redirect_code='s10'))
-    except:
-      pass
-    # ------------------------ send emails function end ------------------------
-  print(' ------------- 100-admin start ------------- ')
-  page_dict = dict(sorted(page_dict.items(),key=lambda x:x[0]))
-  for k,v in page_dict.items():
-    print(f"k: {k} | v: {v}")
-    pass
-  print(' ------------- 100-admin end ------------- ')
-  return render_template('polling/admin_templates/hosts/index.html', page_dict_html=page_dict)
+  return render_template('admin_templates/dashboard/index.html', page_dict_html=page_dict)
 # ------------------------ individual route end ------------------------
