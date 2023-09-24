1dd6ae164dd3fed802bf05a2c79d626f790dda53
Robert <61035696+robertborowski@users.noreply.github.com>
2023-09-24 14:13:38 -0400

    small

---
+++ website/static/javascript/on_page_load/function.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // ----------------------------------- individual start -----------------------------------
+  if ($(window).width() < 1200) {
+    $('#id-remove-custom-vh-95').removeClass('custom-vh-95');
+  }
+  // ----------------------------------- individual end -----------------------------------
+});
