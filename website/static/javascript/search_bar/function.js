8c54a7fdc86087927a4480a87559c0cdd729f6c5
Robert <61035696+robertborowski@users.noreply.github.com>
2023-09-26 20:08:39 -0400

    search bar working

---
+++ website/static/javascript/search_bar/function.js
@@ -0,0 +1,22 @@
+// -------------------------- start --------------------------
+$("#ui_search_bar").keyup(function(){
+  var currently_typed = $(this).val().toLowerCase();
+  currently_typed = $.trim(currently_typed)
+
+  var testList = $('.uiSearchItem');
+  testList.each(function() {
+    var i_category = $(this).text().toLowerCase();
+    i_category = $.trim(i_category)
+    // if (i_category == "aws")
+    if (i_category.indexOf(currently_typed) >= 0) {
+      //  block of code to be executed if the condition is true
+      $( this ).removeClass("uiSearchItemInvisible");
+      $( this ).addClass("uiSearchItemVisible2");
+    } else {
+      //  block of code to be executed if the condition is false
+      $( this ).removeClass("uiSearchItemVisible2");
+      $( this ).addClass("uiSearchItemInvisible");
+    }
+  });
+});
+// -------------------------- end --------------------------
