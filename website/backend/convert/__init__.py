1ebf78819a85971e744816de84498133ba59a3a1
Robert <61035696+robertborowski@users.noreply.github.com>
2023-10-02 07:19:49 -0400

    radio and checkboxes

--- website/backend/convert/__init__.py
+++ website/backend/convert/__init__.py
@@ -22,13 +22,13 @@ def objs_to_arr_of_dicts_function(db_obj, identifier=None):
       char_limit = 35
       i_dict['name_display'] = i_dict['name'][:char_limit]
       # ------------------------ variables for radios and checkboxes start ------------------------
-      i_dict['radio_count_id'] = 'radio_count_' + str(i_counter)
+      i_dict['roles_count_id'] = 'radio_count_' + str(i_counter)
       # ------------------------ variables for radios and checkboxes end ------------------------
     # ------------------------ additional details end ------------------------
     # ------------------------ additional details start ------------------------
     if identifier == 'cv':
       # ------------------------ variables for radios and checkboxes start ------------------------
-      i_dict['checkbox_count_id'] = 'checkbox_count_' + str(i_counter)
+      i_dict['cv_count_id'] = 'checkbox_count_' + str(i_counter)
       # ------------------------ variables for radios and checkboxes end ------------------------
       char_limit = 30
       i_dict['cv_upload_name_display'] = i_dict['cv_upload_name'][:char_limit]
--- website/cv_views_interior_ai.py
+++ website/cv_views_interior_ai.py
@@ -73,10 +73,14 @@ def cv_dashboard_function(url_status_code='one-role-many-cvs', url_redirect_code
     # ------------------------ get user inputs start ------------------------
     ui_one_role_selected = request.form.get('radio-one-role')
     ui_many_cv_selected = request.form.getlist('checkbox-many-cvs')
+    ui_many_roles_selected = request.form.getlist('checkbox-many-roles')
+    ui_one_cv_selected = request.form.get('radio-one-cv')
     # ------------------------ get user inputs end ------------------------
     print(' ------------- 0 ------------- ')
-    print(f"ui_one_role_selected | type: {type(ui_one_role_selected)} | {ui_one_role_selected}")
-    print(f"ui_many_cv_selected | type: {type(ui_many_cv_selected)} | {ui_many_cv_selected}")
+    print(f"1) ui_one_role_selected | type: {type(ui_one_role_selected)} | {ui_one_role_selected}")
+    print(f"2) ui_many_cv_selected | type: {type(ui_many_cv_selected)} | {ui_many_cv_selected}")
+    print(f"3) ui_many_roles_selected | type: {type(ui_many_roles_selected)} | {ui_many_roles_selected}")
+    print(f"4) ui_one_cv_selected | type: {type(ui_one_cv_selected)} | {ui_one_cv_selected}")
     print(' ------------- 0 ------------- ')
     return redirect(url_for('cv_views_interior_ai.cv_dashboard_function', url_status_code='one-role-many-cvs'))
   # ------------------------ post end ------------------------
