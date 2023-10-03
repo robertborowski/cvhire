1c32102b0f811226001f3b8d804e140f3da44d1e
Robert <61035696+robertborowski@users.noreply.github.com>
2023-10-03 08:08:54 -0400

    open ai queue

--- open_ai_queue.py
+++ open_ai_queue.py
@@ -5,7 +5,10 @@ import sendgrid
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, Email, To, Content
 from website.backend.connection import postgres_connect_open_function, postgres_connect_close_function
-from website.backend.sql_queries import select_query_v1_function, select_query_v2_function, select_query_v3_function
+from website.backend.sql_queries import select_query_v1_function, select_query_v2_function, select_query_v3_function, select_query_v4_function, update_query_v1_function
+from website.backend.aws_logic import get_file_contents_from_aws_function
+from website.backend.uploads_user import get_file_suffix_function
+from website.backend.read_files import get_file_contents_function
 # ------------------------ imports end ------------------------
 
 # ------------------------ individual function start ------------------------
@@ -24,6 +27,7 @@ def run_function():
       # ------------------------ select queue start ------------------------
       queue_results_arr_of_dicts = select_query_v1_function(postgres_cursor, 'open_ai_queue_obj')
       # ------------------------ select queue end ------------------------
+      print(' ------------- 0 ------------- ')
       # ------------------------ loop db rows start ------------------------
       for i_queue_dict in queue_results_arr_of_dicts:
         # ------------------------ question type 1 start ------------------------
@@ -33,13 +37,31 @@ def run_function():
           role_dict = role_dict_arr[0]
           # ------------------------ get single end ------------------------
           # ------------------------ get multiple start ------------------------
-          cv_dict_arr = select_query_v3_function(postgres_cursor, 'cv_obj', i_queue_dict['multiple_values'])
-          print(' ------------- 0 ------------- ')
-          print(f"cv_dict_arr | type: {type(cv_dict_arr)} | {cv_dict_arr}")
-          print(' ------------- 0 ------------- ')
+          if i_queue_dict['multiple_values'] == 'select_all_ids':
+            cv_dict_arr = select_query_v4_function(postgres_cursor, 'cv_obj', i_queue_dict['fk_user_id'])
+          else:
+            cv_dict_arr = select_query_v3_function(postgres_cursor, 'cv_obj', i_queue_dict['multiple_values'])
           # ------------------------ get multiple end ------------------------
+          # ------------------------ loop multiple start ------------------------
+          for i_cv_dict in cv_dict_arr:
+            # ------------------------ get content from aws start ------------------------
+            i_file_aws = get_file_contents_from_aws_function(i_cv_dict['cv_aws_id'])
+            # ------------------------ get content from aws end ------------------------
+            # ------------------------ get file suffix start ------------------------
+            file_format_suffix = get_file_suffix_function(i_cv_dict['cv_aws_id'])
+            # ------------------------ get file suffix end ------------------------
+            # ------------------------ read file contents start ------------------------
+            cv_contents = get_file_contents_function(i_file_aws, file_format_suffix)
+            # ------------------------ read file contents end ------------------------
+            # ------------------------ open ai grading start ------------------------
+            # ------------------------ open ai grading end ------------------------
+          # ------------------------ loop multiple end ------------------------
+          # ------------------------ TESTING start ------------------------
+          update_query_v1_function(postgres_connection, postgres_cursor, 'open_ai_queue_obj', 'status', 'closed')
+          # ------------------------ TESTING end ------------------------
         # ------------------------ question type 1 end ------------------------
       # ------------------------ loop db rows end ------------------------
+      print(' ------------- 0 ------------- ')
       # ------------------------ close db connection start ------------------------
       postgres_connect_close_function(postgres_connection, postgres_cursor)
       # ------------------------ close db connection end ------------------------
--- website/backend/sql_queries/__init__.py
+++ website/backend/sql_queries/__init__.py
@@ -1,5 +1,4 @@
 # ------------------------ imports start ------------------------
-import psycopg2.extras
 # ------------------------ imports end ------------------------
 
 # ------------------------ individual function start ------------------------
@@ -25,4 +24,21 @@ def select_query_v2_function(postgres_cursor, var1=None, var2=None):
 def select_query_v3_function(postgres_cursor, var1=None, var2=None):
   sql_query = f"SELECT * FROM {var1} WHERE id IN ({var2});"
   return sql_general_function(postgres_cursor, sql_query)
+# ------------------------ individual function end ------------------------
+
+# ------------------------ individual function start ------------------------
+def select_query_v4_function(postgres_cursor, var1=None, var2=None):
+  sql_query = f"SELECT * FROM {var1} WHERE fk_user_id='{var2}';"
+  return sql_general_function(postgres_cursor, sql_query)
+# ------------------------ individual function end ------------------------
+
+# ------------------------ individual function start ------------------------
+def update_query_v1_function(postgres_connection, postgres_cursor, var1=None, var2=None, var3=None):
+  try:
+    sql_query = f"UPDATE {var1} SET {var2}='{var3}';"
+    postgres_cursor.execute(sql_query)
+    postgres_connection.commit()
+  except:
+    return False
+  return True
 # ------------------------ individual function end ------------------------
