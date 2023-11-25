072f41666c96055b7f1898d27a7a9f29f2fd6cda
Robert <61035696+robertborowski@users.noreply.github.com>
2023-11-25 08:53:28 -0500

    small

---
+++ website/static/javascript/page_flow/function.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // ----------------------------- autoscroll start -----------------------------
+  $("#id-general_autoscroll_click_1").click(function() {
+    var element = document.getElementById('id-general_autoscroll_location_1');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+  // ----------------------------- autoscroll end -----------------------------
+  // ----------------------------- autoscroll start -----------------------------
+  $("#id-general_autoscroll_click_2").click(function() {
+    var element = document.getElementById('id-general_autoscroll_location_2');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+  // ----------------------------- autoscroll end -----------------------------
+  // ----------------------------- autoscroll start -----------------------------
+  $("#id-general_autoscroll_click_3").click(function() {
+    var element = document.getElementById('id-general_autoscroll_location_3');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+  // ----------------------------- autoscroll end -----------------------------
+});
